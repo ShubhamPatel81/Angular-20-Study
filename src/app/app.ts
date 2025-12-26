782a30e0fac292406042c97a7c5b78d64a882c10
Shubham Patel <shubhampatel872005@gmail.com>
2025-12-27 00:19:56 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Admin } from "./component/admin/admin";
+import { User } from "./component/user/user";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // standalone: true,
+
+  imports: [Admin, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
+  ,styles: [`
+    h1 {
+      color: blue;
+    }
+  `]
+
 })
 export class App {
   protected readonly title = signal('project1');
+  name:String='Shyam';
 }
